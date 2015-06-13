266789ed5cf27d4554eb3de6da733f92c38a358e
Joost Baaij <joost@spacebabies.nl>
2015-06-13 22:38:06 +0200

    test file cleanups
    use in-memory sqlite database
    also chuck unused method

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -16,15 +16,6 @@ Mongoid.load!('./config/mongoid.yml')
 
 Dir["models/*.rb"].each {|file| require file }
 
-def assert_not_nil(exp, msg=nil)
-  msg = message(msg) { "<#{mu_pp(exp)}> expected to not be nil" }
-  assert(!exp.nil?, msg)
-end
-
-DB_FILE = 'tmp/test_db'
-FileUtils.mkdir_p File.dirname(DB_FILE)
-FileUtils.rm_f DB_FILE
-
-ActiveRecord::Base.establish_connection :adapter => 'sqlite3', :database => DB_FILE
+ActiveRecord::Base.establish_connection :adapter => 'sqlite3', :database => ":memory:"
 
 load './config/schema.rb'
